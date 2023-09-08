ea52e6c5e21cd0302a0a90c8e9f4e2211e4dc26d
satya-paluri <98354347+satya-paluri@users.noreply.github.com>
2023-09-08 22:01:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  alert("Hello! I am an alert box!!");
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  alert("Hello! I am an alert box!!");
 } catch (error) {
   core.setFailed(error.message);
 }
